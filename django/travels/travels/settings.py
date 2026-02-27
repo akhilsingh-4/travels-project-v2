9ea85b23e1cc30ad16db82425a439d4ddbc06a53
Akhil Singh <akhilsingh@fxis.ai>
2026-02-27 12:44:22 +0530

    Remove SessionAuthentication to fix CSRF issues in production

--- django/travels/travels/settings.py
+++ django/travels/travels/settings.py
@@ -159,7 +159,6 @@ STATIC_ROOT = BASE_DIR / "staticfiles"
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [
         'rest_framework_simplejwt.authentication.JWTAuthentication',
-        'rest_framework.authentication.SessionAuthentication',
     ]
 }
 
